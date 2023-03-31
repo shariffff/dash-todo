6974325fd2aae5dba24801ec26e3b3c3fb91da61
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2023-03-31 19:22:34 +0600

    1.0.1 build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-react-refresh-runtime'), 'version' => '1bf196342810c9f1b8f6');
+<?php return array('dependencies' => array('react-dom', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'd8279c4a51add1514836');
