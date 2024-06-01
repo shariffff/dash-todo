a73bb49a6a53afb26b0ae0bb9e3002d042565f96
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2024-06-01 19:31:21 +0600

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,20 @@ import {
 	useQueryClient,
 	QueryClient,
 	QueryClientProvider,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-const queryClient = new QueryClient()
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
 
 export default function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
+		<QueryClientProvider client={ queryClient }>
 			<div className="dash-todo-layout">
 				<div className="dash-todo-layout-body hide-if-no-js">
 					<AddTodo />
 					<TodoList />
 				</div>
 			</div>
-			<ReactQueryDevtools initialIsOpen={false} />
+			<ReactQueryDevtools initialIsOpen={ false } />
 		</QueryClientProvider>
-
 	);
 }
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -9,110 +9,109 @@ import {
 	__experimentalToggleGroupControl as ToggleGroupControl,
 	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
 } from '@wordpress/components';
-import {
-	useMutation,
-	useQueryClient,
-} from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 
-
 export default function AddTodo() {
-	const [date, setDate] = useState();
-	const [todo, setTodo] = useState('');
-	const [priority, setPriority] = useState('0');
+	const [ date, setDate ] = useState();
+	const [ todo, setTodo ] = useState( '' );
+	const [ priority, setPriority ] = useState( '0' );
 
 	const createTodo = () => {
-		mutation.mutate({
+		mutation.mutate( {
 			title: todo,
 			status: 'pending',
 			excerpt: date,
 			menu_order: priority,
-		})
-	}
+		} );
+	};
 
-	const queryClient = useQueryClient()
+	const queryClient = useQueryClient();
 
-	const mutation = useMutation({
-		mutationFn: (newTodo) => apiFetch({
-			path: '/wp/v2/todo',
-			method: 'POST',
-			data: newTodo,
-		}),
+	const mutation = useMutation( {
+		mutationFn: ( newTodo ) =>
+			apiFetch( {
+				path: '/wp/v2/todo',
+				method: 'POST',
+				data: newTodo,
+			} ),
 		onSuccess: () => {
-			setTodo('')
-			queryClient.invalidateQueries('todos')
+			setTodo( '' );
+			queryClient.invalidateQueries( 'todos' );
 		},
-	})
-	const handleKeyDown = (event) => {
-		if (event.key === 'Enter') {
-			handleFormSubmit(event);
+	} );
+	const handleKeyDown = ( event ) => {
+		if ( event.key === 'Enter' ) {
+			handleFormSubmit( event );
 		}
 	};
 
-	const handleFormSubmit = (event) => {
+	const handleFormSubmit = ( event ) => {
 		event.preventDefault();
-		if (todo.trim().length !== 0) {
+		if ( todo.trim().length !== 0 ) {
 			createTodo();
 		}
 	};
 	const resetDatePriority = () => {
-		setDate(null);
-		setPriority('0');
+		setDate( null );
+		setPriority( '0' );
 	};
 
 	return (
-		<Card style={{ boxShadow: 'none' }}>
+		<Card style={ { boxShadow: 'none' } }>
 			<CardBody>
-				<form onSubmit={createTodo}>
+				<form onSubmit={ createTodo }>
 					<TextControl
 						autoFocus
-						label={'Add Todo item'}
+						label={ 'Add Todo item' }
 						hideLabelFromVision
 						placeholder="Type here and press Enter ↵"
-						onChange={(value) => setTodo(value)}
-						value={todo}
-						onKeyDown={handleKeyDown}
+						onChange={ ( value ) => setTodo( value ) }
+						value={ todo }
+						onKeyDown={ handleKeyDown }
 						autoComplete="off"
-						style={{ fontSize: 15, minHeight: 36 }}
+						style={ { fontSize: 15, minHeight: 36 } }
 					/>
 					<Flex>
-						<Flex justify='flex-start' align='start'>
+						<Flex justify="flex-start" align="start">
 							<Dropdown
-								popoverProps={{ placement: 'bottom-start' }}
-								renderToggle={({ isOpen, onToggle }) => (
+								popoverProps={ { placement: 'bottom-start' } }
+								renderToggle={ ( { isOpen, onToggle } ) => (
 									<Button
 										variant="tertiary"
-										onClick={onToggle}
-										aria-expanded={isOpen}
+										onClick={ onToggle }
+										aria-expanded={ isOpen }
 										icon="calendar-alt"
-										style={{ backgroundColor: 'rgba(56, 88, 233, 0.4)', minWidth: 64 }}
+										style={ {
+											backgroundColor:
+												'rgba(56, 88, 233, 0.4)',
+											minWidth: 64,
+										} }
 									>
-										{date &&
-											new Date(date)
+										{ date &&
+											new Date( date )
 												?.getDate()
-												?.toString()}
+												?.toString() }
 									</Button>
-								)}
-								renderContent={() => (
+								) }
+								renderContent={ () => (
 									<DatePicker
-										style={{ padding: 10 }}
-										currentDate={date}
-										onChange={(newDate) =>
-											setDate(newDate)
+										style={ { padding: 10 } }
+										currentDate={ date }
+										onChange={ ( newDate ) =>
+											setDate( newDate )
 										}
 									/>
-								)}
+								) }
 							/>
 
 							<ToggleGroupControl
 								isBlock
 								hideLabelFromVision
 								label="Priority"
-								value={priority}
-								onChange={(value) =>
-									setPriority(value)
-								}
+								value={ priority }
+								onChange={ ( value ) => setPriority( value ) }
 							>
 								<ToggleGroupControlOption
 									value="3"
@@ -134,7 +133,7 @@ export default function AddTodo() {
 						</Flex>
 						<Button
 							variant="tertiary"
-							onClick={resetDatePriority}
+							onClick={ resetDatePriority }
 						>
 							Reset
 						</Button>
