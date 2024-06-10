f3e5579d7aea4ec7e3280698cccd470d4f8fbbff
Sharif Mohammad Eunus <sharif.eunus@gmail.com>
2024-06-10 08:45:23 +0600

    wip

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,20 @@
 <?php
+/**
+ * Uninstall the plugin
+ *
+ * @package DashTodo
+ */
 
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	die;
 }
 
 $all_todo = get_posts(
-	[ 
-		'post_type' => 'todo',
+	array(
+		'post_type'   => 'todo',
 		'numberposts' => -1,
-		'fields' => 'ids'
-	]
+		'fields'      => 'ids',
+	)
 );
 foreach ( $all_todo as $todo ) {
 	wp_delete_post( $todo, true );
